cb93f0bda8f36532aa52b6a4c57b567e694c4a95
mrLevin778 <vitaliy.respect@gmail.com>
2022-09-21 22:35:13 +0300

    fix date format

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from typing import List, Any
-import itertools
+import datetime
 import requests
 from bs4 import BeautifulSoup
 
@@ -7,9 +7,11 @@ siteAddress = "https://www.kijiji.ca"
 siteToronto = "/b-apartments-condos/city-of-toronto/page-"
 siteCategory = "/c37l1700273"
 sitePageNum = 0
-
-
-# <a href="/b-apartments-condos/city-of-toronto/page-2/c37l1700273">2</a> - pages
+allPages = []  # list of all pages
+lastPage = 94  # temporary variable
+dt = datetime.datetime.today()
+today = dt.strftime("%d-%m-%Y")
+elementId = 0
 
 # now this class don't use
 class Apartment:
@@ -48,10 +50,8 @@ headers = {
     'accept-language': 'en-US,en;q=0.9',
 }
 
-allPages = []  # list of all pages
-lastPage = 94  # temporary variable
-
 page = requests.get(siteAddress + siteToronto + str(sitePageNum) + siteCategory)
+
 # extract number of pages, now it's don't work :(
 while True:
     sitePageNum += 1
@@ -92,16 +92,13 @@ if page.status_code == 200:
     allTitleItems = soup.find_all('div', attrs={"class": "title"})
     clearTitleItems = extractTextFromTags(allTitleItems)
 
-    # extract date
-    # allDateItems = soup.find_all('span', attrs={"class": "date-posted"})
-    # clearDateItems = extractTextFromTags(allDateItems)
-
     # extract number of bedrooms
     allBedsNumItems = soup.find_all('span', attrs={"class": "bedrooms"})
     clearBedsNumItemsTemp = extractTextFromTags(allBedsNumItems)
     clearBedsNumItems = []
     for item in clearBedsNumItemsTemp:
         clearItemTemp = (item[5:])
+        clearItem = ''
         for item in range(len(clearItemTemp)):
             if clearItemTemp[item].isdigit():
                 clearItem = ''
@@ -121,28 +118,21 @@ if page.status_code == 200:
     clearDateItems = []
     for item in clearLocationItemsTemp:
         clearItem = (item[-10:])
+        if clearItem == 'oYesterday':
+            clearItem = today
+        else:
+            clearItem = clearItem.replace('/', '-')
         clearDateItems.append(clearItem)
 
     # print(allImageURLItems[5])
-    print(f'Price: ' + clearPriceItems[5])
-    print(f'Description: ' + clearDescriptionItems[5])
-    print(f'Title: ' + clearTitleItems[5])
-    print(f'Date: ' + clearDateItems[5])
-    print(f'Bedrooms: ' + clearBedsNumItems[5])
-    print(f'Location: ' + clearLocationItems[5])
-
-    # for data in allItems:
-    #    if data.find('div', class_='left-col') is not None:
-    #        filteredItems.append(data.text)
+    print(f'Price: ' + clearPriceItems[elementId])
+    print(f'Description: ' + clearDescriptionItems[elementId])
+    print(f'Title: ' + clearTitleItems[elementId])
+    print(f'Date: ' + clearDateItems[elementId])
+    print(f'Bedrooms: ' + clearBedsNumItems[elementId])
+    print(f'Location: ' + clearLocationItems[elementId])
 
     # for element in soup.find_all('div', class_='clearfix'):
-    # imageURL = element.find('img').get('data-src')
-    # price = element.find('div', class_='price').text.strip()
-    # description = element.find('div', class_='description').text.strip()
-    # title = element.find('div', class_='title').text.strip()
-    # date = element.find('span', class_='date-posted').text.strip()
-    # bedsNum = element.find('span', class_='bedrooms').text.strip()
-    # location = element.find('div', class_='location').text.strip()
 
     # apart = Apartment(imageurl, price, description, title, date, bedsnum, location)
 
