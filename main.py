7890f229208dcc5c2f059ba4bd9bc4bd95fbbfee
mrLevin778 <vitaliy.respect@gmail.com>
2022-09-23 01:17:41 +0300

    creating lists with all items

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ sitePageNum = 83
 site_first_page = 'https://www.kijiji.ca/b-apartments-condos/city-of-toronto/c37l1700273?ad=offering'
 allPages = []  # list of all pages
 elementId = 0  # test element
+is_last_page = True
 
 
 # lists for data from all pages
@@ -60,8 +61,7 @@ page = requests.get(siteAddress + siteToronto + str(sitePageNum) + siteCategory)
 
 
 # main function
-while True:
-    is_last_page = False
+while is_last_page == True:
     sitePageNum += 1
     #allPages.append(siteAddress + siteToronto + str(sitePageNum) + siteCategory)
     #if siteURL == exact_url:
@@ -80,91 +80,105 @@ while True:
         soup = BeautifulSoup(response.text, 'html.parser')
         print(f'Page url: ' + str(siteURL))
         if siteURL != exact_url and exact_url != site_first_page:
-            is_last_page = True
-        print(f'Flag: ' + str(is_last_page))
+            is_last_page = False
+        else:
+            print(f'Flag: ' + str(is_last_page))
 
-        # extract price
-        allPriceItems = soup.find_all('div', attrs={"class": "price"})
-        clearPriceItems = extractTextFromTags(allPriceItems)
+            # extract price
+            allPriceItems = soup.find_all('div', attrs={"class": "price"})
+            clearPriceItems = extractTextFromTags(allPriceItems)
 
-        # extract description
-        allDescriptionItems = soup.find_all('div', attrs={"class": "description"})
-        clearDescriptionItems = extractTextFromTags(allDescriptionItems)
+            # extract description
+            allDescriptionItems = soup.find_all('div', attrs={"class": "description"})
+            clearDescriptionItems = extractTextFromTags(allDescriptionItems)
 
-        # extract title
-        allTitleItems = soup.find_all('div', attrs={"class": "title"})
-        clearTitleItems = extractTextFromTags(allTitleItems)
+            # extract title
+            allTitleItems = soup.find_all('div', attrs={"class": "title"})
+            clearTitleItems = extractTextFromTags(allTitleItems)
 
-        # extract number of bedrooms
-        allBedsNumItems = soup.find_all('span', attrs={"class": "bedrooms"})
-        clearBedsNumItemsTemp = extractTextFromTags(allBedsNumItems)
-        clearBedsNumItems = []
-        for item in clearBedsNumItemsTemp:
-            clearItemTemp = (item[5:])
-            clearItem = ''
-            for item in range(len(clearItemTemp)):
-                if clearItemTemp[item].isdigit():
-                    clearItem = ''
-                    clearItem += str(clearItemTemp[item])
-            clearBedsNumItems.append(clearItem)
+            # extract number of bedrooms
+            allBedsNumItems = soup.find_all('span', attrs={"class": "bedrooms"})
+            clearBedsNumItemsTemp = extractTextFromTags(allBedsNumItems)
+            clearBedsNumItems = []
+            for item in clearBedsNumItemsTemp:
+                clearItemTemp = (item[5:])
+                clearItem = ''
+                for item in range(len(clearItemTemp)):
+                    if clearItemTemp[item].isdigit():
+                        clearItem = ''
+                        clearItem += str(clearItemTemp[item])
+                clearBedsNumItems.append(clearItem)
 
-        # extract location
-        allLocationItems = soup.find_all('div', attrs={"class": "location"})
-        clearLocationItemsTemp = extractTextFromTags(allLocationItems)
-        clearLocationItems = []
-        for item in clearLocationItemsTemp:
-            if item.endswith('Yesterday'):
-                clearLocationItem = (item[:-9])
-            elif item.endswith('hours ago'):
-                clearLocationItem = (item[:-12])
-            elif item.endswith('minutes ago'):
-                clearLocationItem = (item[:-14])
-            else:
-                clearLocationItem = (item[:-10])
-            clearLocationItems.append(clearLocationItem)
+            # extract location
+            allLocationItems = soup.find_all('div', attrs={"class": "location"})
+            clearLocationItemsTemp = extractTextFromTags(allLocationItems)
+            clearLocationItems = []
+            for item in clearLocationItemsTemp:
+                if item.endswith('Yesterday'):
+                    clearLocationItem = (item[:-9])
+                elif item.endswith('hours ago'):
+                    clearLocationItem = (item[:-12])
+                elif item.endswith('minutes ago'):
+                    clearLocationItem = (item[:-14])
+                else:
+                    clearLocationItem = (item[:-10])
+                clearLocationItems.append(clearLocationItem)
 
-        # alternate extract date
-        clearDateItems = []
-        for item in clearLocationItemsTemp:
-            clearItem = (item[-10:])
-            yesterday = 'y'
-            today = 't'
-            if clearItem.endswith('Yesterday'):
-                clearItem = get_dates(yesterday)
-            elif clearItem.endswith(' ago'):
-                clearItem = get_dates(today)
-            else:
-                clearItem = clearItem.replace('/', '-')
-            clearDateItems.append(clearItem)
+            # alternate extract date
+            clearDateItems = []
+            for item in clearLocationItemsTemp:
+                clearItem = (item[-10:])
+                yesterday = 'y'
+                today = 't'
+                if clearItem.endswith('Yesterday'):
+                    clearItem = get_dates(yesterday)
+                elif clearItem.endswith(' ago'):
+                    clearItem = get_dates(today)
+                else:
+                    clearItem = clearItem.replace('/', '-')
+                clearDateItems.append(clearItem)
 
-        # extract image URL
-        allImageURLItems = soup.find_all('div', attrs={"class": "image"})
-        clearImageURLItems = []
-        for i in allImageURLItems:
-            i = str(i)
-            soup = BeautifulSoup(i, 'html.parser')
-            images = soup.find_all('img')
-            for image in images:
-                try:
-                    imageURL = image['data-src']
-                except:
-                    imageURL = image['src']
-                clearImageURLItems.append(imageURL)
+            # extract image URL
+            allImageURLItems = soup.find_all('div', attrs={"class": "image"})
+            clearImageURLItems = []
+            for i in allImageURLItems:
+                i = str(i)
+                soup = BeautifulSoup(i, 'html.parser')
+                images = soup.find_all('img')
+                for image in images:
+                    try:
+                        imageURL = image['data-src']
+                    except:
+                        imageURL = image['src']
+                    clearImageURLItems.append(imageURL)
 
-        print(f'Items on page: ' + str(len(clearBedsNumItems)))
+            print(f'Items on page: ' + str(len(clearBedsNumItems)))
 
-        print(f'Image URL: ' + str(clearImageURLItems[elementId]))
-        print(f'Price: ' + clearPriceItems[elementId])
-        print(f'Description: ' + clearDescriptionItems[elementId])
-        print(f'Title: ' + clearTitleItems[elementId])
-        print(f'Date: ' + clearDateItems[elementId])
-        print(f'Bedrooms: ' + clearBedsNumItems[elementId])
-        print(f'Location: ' + clearLocationItems[elementId])
+            # get elements from pages and add into common list
+            print(f'Image URL: ' + str(clearImageURLItems[elementId]))
+            for i in clearImageURLItems:
+                image_url_items.append(i)
+            print(f'Price: ' + clearPriceItems[elementId])
+            for i in clearPriceItems:
+                price_items.append(i)
+            print(f'Description: ' + clearDescriptionItems[elementId])
+            for i in clearDescriptionItems:
+                description_items.append(i)
+            print(f'Title: ' + clearTitleItems[elementId])
+            for i in clearTitleItems:
+                title_items.append(i)
+            print(f'Date: ' + clearDateItems[elementId])
+            for i in clearDateItems:
+                date_items.append(i)
+            print(f'Bedrooms: ' + clearBedsNumItems[elementId])
+            for i in clearBedsNumItems:
+                bedrooms_items.append(i)
+            print(f'Location: ' + clearLocationItems[elementId])
+            for i in clearLocationItems:
+                location_items.append(i)
 
-        # for element in soup.find_all('div', class_='clearfix'):
-
-        # apart = Apartment(imageurl, price, description, title, date, bedsnum, location)
-
-        # print(apart.__repr__())
+    elif page.status_code == 500:
+        print(f'Server Error!')
     else:
-        print('Connection Error or end of pages!')
+        print('Some Connection Error!')
+print(f'Items num: ' + str(len(image_url_items)))
